28effc301fd2d978a23ded9b95e7e3bcfa346c85
TABET DJALAL <dtabet@oppido.fr>
2020-03-21 23:15:40 +0100

    configure import

--- src/main/java/com/userfront/domain/PrimaryAccount.java
+++ src/main/java/com/userfront/domain/PrimaryAccount.java
@@ -2,7 +2,13 @@ package com.userfront.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
