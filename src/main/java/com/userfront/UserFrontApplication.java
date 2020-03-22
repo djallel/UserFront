a593c6ec042331297b08697ad0acecd272ed7ae1
TABET DJALAL <dtabet@oppido.fr>
2020-03-22 16:43:48 +0100

    War pom jenkins

--- src/main/java/com/userfront/UserFrontApplication.java
+++ src/main/java/com/userfront/UserFrontApplication.java
@@ -3,9 +3,10 @@ package com.userfront;
 import com.userfront.brouillon.Triangle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserFrontApplication {
+public class UserFrontApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserFrontApplication.class, args);
